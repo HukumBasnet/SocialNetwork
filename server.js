b19a1575058de16b77700955702d0518c61457f0
reenahukum <reenareenahukum123@gmail.com>
2019-12-09 13:37:16 +0545

    adding require routes

--- server.js
+++ server.js
@@ -14,4 +14,15 @@ app.get("/", (req, res) => {
     res.send("API Running")
 })
 
+//Load api
+const Postapi = require('./routes/api/posts');
+const authapi = require('./routes/api/auth');
+const profileapi = require('./routes/api/profile')
+const usersapi = require('./routes/api/users');
+
+app.use('api/user', usersapi);
+app.use('api/auth', authapi);
+app.use('api/profile', profileapi);
+app.use('api/posts', Postapi)
+
 app.listen(PORT, () => console.log(`Server is listening at ${ PORT }`));
