26f1e6074e73a5bef0097e29fcc38a0c26b26b76
reenahukum <reenareenahukum123@gmail.com>
2019-12-15 14:32:04 +0545

    adding initial component

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,94 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Register = () => {
+    const [ formData, setFormData ] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name] : e.target.value });
+
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log(formData);
+        }
+    };
+
+
+
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign up</h1>
+            <p className="lead">
+                <i className='fas fa-user'/> Create Your Account
+            </p>
+            <form className='form' onSubmit = { e => onSubmit(e) }>
+               <div className='form-group'>
+                    <input 
+                        type='text'
+                        placeholder = 'Name'
+                        name = 'name'
+                        value = { name}
+                        onChange = { e => onChange(e) }
+                        required   
+                   />
+                </div> 
+
+               
+
+                <div className='form-group'>
+                    <input 
+                        type='email'
+                        placeholder = 'Email'
+                        name = 'email'
+                        value = { email}
+                        onChange = { e => onChange(e) }
+                        required   
+                   />
+                </div> 
+
+                <div className='form-group'>
+                    <input 
+                        type='password'
+                        placeholder = 'password'
+                        name = 'password'
+                        value = { password}
+                        onChange = { e => onChange(e) }
+                        minLength = '6' 
+                   />
+                </div> 
+
+                <div className='form-group'>
+                    <input 
+                        type='password'
+                        placeholder = 'Confirm password'
+                        name = 'password2'
+                        value = { password2}
+                        onChange = { e => onChange(e) }
+                        minLength = '6' 
+                   />
+                </div> 
+                <input type = 'submit' className = 'btn btn-primary' value='Register' />
+            </form>
+            <p className ='my-1'>
+                Already have an account ? <Link to = '/login'> Sign in</Link>
+            </p>
+        </Fragment>
+      
+    )
+}
+
+export default Register
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='navbar bg-dark'>
+                <h1>
+                    <Link to = '/'>
+                        <i className='fas fa-code' /> Socail Network
+                    </Link>
+                </h1>
+                <ul>
+                   <li><a href='!#' > Developers </a></li>
+                   <li> <Link to='/register'>Register</Link> </li>
+
+                   <li> <Link to = '/login'> Login</Link></li> 
+                </ul>
+            </nav>            
+        </div>
+    )
+}
+
+export default Navbar
